cc2d51ee0eabf3f438aaeaddd07816e9f80b4ec4
graeme.castle <graeme.castle@rbc.com>
2018-04-20 15:11:24 -0400

    Added traversal for v22

---
+++ app/src/main/java/gcastle/com/accessibilitydemo/V22TraversalActivity.java
@@ -0,0 +1,15 @@
+package gcastle.com.accessibilitydemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class V22TraversalActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_v22_traversal);
+    }
+}
